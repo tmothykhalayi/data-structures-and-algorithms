1adcd2bee0e2f07943e5a74310079a4a1ae5fc0d
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2026-02-17 00:22:10 +0300

    Add twoSum function implementation for solving the two-sum problem

---
+++ phase-1-linear-data-structures/arrays/01.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+  const map = new Map();  
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    
+    map.set(nums[i], i);
+  }
+  
+  return null;
+}
+console.log(twoSum([2, 7, 11, 15], 9));
